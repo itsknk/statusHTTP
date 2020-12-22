c3d2cf091dc7fd07ba69f56efb73b0d2e57eace3
itsknk <kishorekaranam99@gmail.com>
2020-12-22 19:41:57 +0530

    comments made decent

--- errorFunc.go
+++ errorFunc.go
@@ -4,32 +4,32 @@ import (
 	"net/http"
 )
 
-//Unauthorized ...
+// Unauthorized for 401
 func Unauthorized(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 401, message...)
 }
 
-//Forbidden ...
+// Forbidden for 403
 func Forbidden(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 403, message...)
 }
 
-//BadRequest ...
+// BadRequest for 400
 func BadRequest(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 400, message...)
 }
 
-//Internal Server Error
+// Internal Server Error for 500
 func Internal(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 500, message...)
 }
 
-//NotFound ...
+// NotFound for 404
 func NotFound(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 404, message...)
 }
 
-//BadGateway ...
+// BadGateway for 502
 func BadGateway(w http.ResponseWriter, message ...interface{}) {
 	statusHTTP(w, 502, message...)
 }
