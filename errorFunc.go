6b308ca0734f3b50856be6374c4bd3ef1025e0eb
itsknk <kishorekaranam99@gmail.com>
2020-12-22 19:01:59 +0530

    added error functions

---
+++ errorFunc.go
@@ -0,0 +1,31 @@
+package statusHTTP
+
+import (
+	"net/http"
+)
+
+//Unauthorized
+func Unauthorized(w http.ResponseWriter, message ...interface{}) {
+	statusHTTP(w, 401, message...)
+}
+
+//Forbidden
+func Forbidden(w http.ResponseWriter, message ...interface{}) {
+	statusHTTP(w, 403, message...)
+}
+
+//BadRequest
+func BadRequest(w http.ResponseWriter, message ...interface{}) {
+	statusHTTP(w, 400, message...)
+}
+
+//Internal Server Error
+func Internal(w http.ResponseWriter, message ...interface{}) {
+	statusHTTP(w, 500, message...)
+}
+
+//NotFound
+func NotFound(w http.ResponseWriter, message ...interface{}) {
+	statusHTTP(w, 404, message...)
+}
+
